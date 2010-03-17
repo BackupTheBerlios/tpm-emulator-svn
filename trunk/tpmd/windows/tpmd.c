c7d06ec441f5ff6730d517857c7bf9572334f5fe
mast <mast@66d3cfb4-bce7-0310-a3bb-9577955f36f5>
2010-03-17 22:14:21 +0000

    tpmd updated

--- trunk/tpmd/windows/tpmd.c
+++ trunk/tpmd/windows/tpmd.c
@@ -36,31 +36,20 @@ static int is_service = 0;
 static int opt_debug = 0;
 static int opt_foreground = 0;
 static const char *opt_pipe_name = TPM_DEVICE_NAME;
-static const char *opt_storage_file = TPM_STORAGE_NAME;
-static const char *opt_log_file = TPM_LOG_FILE;
 static int tpm_startup = 2;
 static uint32_t tpm_config = 0;
-static HCRYPTPROV rand_ch;
+extern const char *tpm_storage_file;
+extern const char *tpm_log_file;
 static SERVICE_STATUS_HANDLE status_handle;
 static DWORD current_status;
 
-void *tpm_malloc(size_t size)
-{
-  return malloc(size);
-}
-
-void tpm_free(/*const*/ void *ptr)
-{
-  if (ptr != NULL) free((void*)ptr);
-}
-
-void tpm_log(int priority, const char *fmt, ...)
+void my_log(int priority, const char *fmt, ...)
 {
     FILE *file;
     va_list ap, bp;
     va_start(ap, fmt);
     va_copy(bp, ap);
-    file = fopen(opt_log_file, "a");
+    file = fopen(tpm_log_file, "a");
     if (file != NULL) {
         vfprintf(file, fmt, ap);
         fclose(file);
@@ -72,82 +61,15 @@ void tpm_log(int priority, const char *fmt, ...)
     va_end(bp);
 }
 
-void tpm_get_extern_random_bytes(void *buf, size_t nbytes)
-{
-  CryptGenRandom(rand_ch, nbytes, (BYTE*)buf);
-}
-
-uint64_t tpm_get_ticks(void)
-{
-    static uint64_t old_t = 0;
-    uint64_t new_t, res_t;
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    new_t = (uint64_t)tv.tv_sec * 1000000 + (uint64_t)tv.tv_usec;
-    res_t = (old_t > 0) ? new_t - old_t : 0;
-    old_t = new_t;
-    return res_t;
-}
-
-int tpm_write_to_storage(uint8_t *data, size_t data_length)
-{
-    int fh;
-    ssize_t res;
-    fh = open(opt_storage_file, O_WRONLY | O_TRUNC | O_CREAT | O_BINARY,
-              S_IRUSR | S_IWUSR);
-    if (fh < 0) return -1;
-    while (data_length > 0) {
-        res = write(fh, data, data_length);
-        if (res < 0) {
-            close(fh);
-            return -1;
-        }
-        data_length -= res; 
-        data += res;
-    }
-    close(fh);
-    return 0;
-}
-
-int tpm_read_from_storage(uint8_t **data, size_t *data_length)
-{
-    int fh;
-    ssize_t res;
-    size_t total_length;
-    fh = open(opt_storage_file, O_RDONLY | O_BINARY);
-    if (fh < 0) return -1;
-    total_length = lseek(fh, 0, SEEK_END);
-    lseek(fh, 0, SEEK_SET);
-    *data = tpm_malloc(total_length);
-    if (*data == NULL) {
-        close(fh);
-        return -1;
-    }
-    *data_length = 0;
-    while (total_length > 0) {
-        res = read(fh, &(*data)[*data_length], total_length);
-        if (res < 0) {
-            close(fh);
-            tpm_free(*data);
-            return -1;
-        }
-        if (res == 0) break;
-        *data_length += res;
-        total_length -= res;
-    }
-    close(fh);
-    return 0;
-}
-
 static void print_usage(char *name)
 {
     printf("usage: %s [-d] [-f] [-s storage file] [-u windows pipe name] "
            "[-l log file] [-h] [startup mode]\n", name);
     printf("  d : enable debug mode\n");
     printf("  f : forces the application to run in the foreground\n");
-    printf("  s : storage file to use (default: %s)\n", opt_storage_file);
+    printf("  s : storage file to use (default: %s)\n", tpm_storage_file);
     printf("  u : windows named pipe name to use (default: %s)\n", opt_pipe_name);
-    printf("  l : name of the log file (default: %s)\n", opt_log_file);
+    printf("  l : name of the log file (default: %s)\n", tpm_log_file);
     printf("  h : print this help message\n");
     printf("  startup mode : must be 'clear', "
            "'save' (default) or 'deactivated\n");
@@ -169,16 +91,16 @@ static int parse_options(int argc, char **argv)
                 opt_foreground = 1;
                 break;
             case 's':
-                opt_storage_file = optarg;
-                debug("using storage file '%s'", opt_storage_file);
+                tpm_storage_file = optarg;
+                debug("using storage file '%s'", tpm_storage_file);
                 break;
             case 'u':
                 opt_pipe_name = optarg;
                 debug("using named pipe '%s'", opt_pipe_name);
                 break;
             case 'l':
-                opt_log_file = optarg;
-                debug("using log file '%s'", opt_log_file);
+                tpm_log_file = optarg;
+                debug("using log file '%s'", tpm_log_file);
                 break;
             case 'c':
                 tpm_config = strtol(optarg, NULL, 0);
@@ -210,7 +132,7 @@ static int parse_options(int argc, char **argv)
     } else {
         /* if no startup mode is given assume save if a configuration
            file is available, clear otherwise */
-        int fh = open(opt_storage_file, O_RDONLY);
+        int fh = open(tpm_storage_file, O_RDONLY);
         if (fh < 0) {
             tpm_startup = 1;
             info("no startup mode was specified; asuming 'clear'");
@@ -269,23 +191,6 @@ static int init_signal_handler(void)
     return 0;
 }
 
-static int mkdirs(const char *path)
-{
-    char *copy = strdup(path);
-    char *p = strchr(copy + 1, '/');
-    while (p != NULL) {
-        *p = '\0';
-        if ((mkdir(copy) == -1) && (errno != EEXIST)) {
-            free(copy);
-            return errno;
-        }
-        *p = '/';
-        p = strchr(p + 1, '/');
-    }
-    free(copy);
-    return 0;
-}
-
 static void main_loop(void)
 {
     HANDLE ph;
@@ -301,13 +206,16 @@ static void main_loop(void)
       PIPE_UNLIMITED_INSTANCES, TPM_CMD_BUF_SIZE,
       TPM_CMD_BUF_SIZE, 0, NULL);
     if (ph == INVALID_HANDLE_VALUE) {
-         error("CreateNamedPipe() failed: %s", get_error());
-         return;
+        error("CreateNamedPipe() failed: %s", get_error());
+        return;
     }
     /* init tpm emulator */
-    mkdirs(opt_storage_file);
     debug("initializing TPM emulator");
-    tpm_emulator_init(tpm_startup, tpm_config);
+    if (tpm_emulator_init(tpm_startup, tpm_config) != 0) {
+        error("tpm_emulator_init() failed");
+        CloseHandle(ph);
+        return;
+    }
     /* start command processing */
     while (!stopflag) {
         /* wait for incomming connections */
@@ -407,8 +315,6 @@ void serviceMain(int argc, char **argv)
                             ERROR_SERVICE_SPECIFIC_ERROR, 1, 0, 0);
         return;
     }
-    /* init logging */
-    mkdirs(opt_log_file);
     /* init signal handler */
     if (init_signal_handler() != 0) {
         updateServiceStatus(SERVICE_STOPPED,
