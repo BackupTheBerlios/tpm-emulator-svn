9bd75ed640c4091957124ec5bc40a79cb0fa895e
mast <mast@66d3cfb4-bce7-0310-a3bb-9577955f36f5>
2010-12-30 09:46:43 +0000

    path for win7 added

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set BUILD_DIR=build
-set PATH=C:\MinGW\bin;C:\Program Files\CMake 2.8\bin;%PATH%
+set PATH=C:\MinGW\bin;C:\Program Files\CMake 2.8\bin;C:\Program Files (x86)\CMake 2.8\bin;%PATH%
 
 if "%1" == "clean" rmdir "%BUILD_DIR%" /S /Q
 
@@ -10,6 +10,6 @@ if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 cd %BUILD_DIR%
 cmake .. -G "MinGW Makefiles"
 ::cmake .. -G "MinGW Makefiles" -DMTM_EMULATOR=ON
-make
+mingw32-make
 cd ..
 
